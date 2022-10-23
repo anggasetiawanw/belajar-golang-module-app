aab73c664cd6cacb1ce72e70d5f5c26b20b59542
anggasetiawanw <anggasetiaw@gmail.com>
2022-10-23 17:39:36 +0700

    consume go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/anggasetiawanw/belajar-golang-module-app
+
+go 1.19
+
+require github.com/anggasetiawanw/belajar-golang-module v1.0.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+	go_say_hello "github.com/anggasetiawanw/belajar-golang-module"
+)
+
+func main()  {
+	fmt.Println(go_say_hello.SayHello("Angga Setiawan Wahyudin"))
+}
